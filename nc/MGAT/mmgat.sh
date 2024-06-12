cf476af7e12fdcb9add90098170a640da2145f23
Jing Zhu <jingzhuu@umich.edu>
2024-06-11 21:32:45 -0400

    update sh

--- nc/MGAT/mmgat.sh
+++ nc/MGAT/mmgat.sh
@@ -17,16 +17,3 @@ python run.py \
     # --do_hyperparameter_search
 
 
-#srun --time=72:00:00 --qos=huge-long --partition=clip --account=clip \
-#--gres=gpu:rtxa6000:1 --cpus-per-task=4 --mem=20g \
-#python run.py --weight_decay 1e-1 --batch_size 2048 --num_epoch 20 \
-#--edge_split_path ../../../../Patton/books-lp/lp-edge-split-random.pt \
-#--feat_path ../../../../Patton/books-lp/t5vit_feat.pt \
-#--project_name mmgat_book \
-#--wandb_run_name random_t5vit \
-#--PATH_best_weight_save './mmgat_book_ckpt_t5.pt' \
-#--PATH_best_metrics_save './mmgat_book_best_metrics_t5.txt' \
-#--PATH_best_hyperparameter_save './mmgat_book_best_hyperparameter_t5.txt' \
-#--num_layers 2 \
-#--l_r 0.002 \
-#--do_hyperparameter_search \
